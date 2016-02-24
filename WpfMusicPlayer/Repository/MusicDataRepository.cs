88c5e2acb59767f66d22c9466c589ad08207fae2
alexpbrandao <alexpbrandao@users.noreply.github.com>
2016-02-23 22:44:05 -0800

    Loading json file from internet
    Modified repository to load the file from the internet asynchronously.
    If it somehow fails, it loads a local copy of the json file from the assets folder.

--- WpfMusicPlayer/Repository/MusicDataRepository.cs
+++ WpfMusicPlayer/Repository/MusicDataRepository.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
+using System.Windows;
 using Newtonsoft.Json;
 using WpfMusicPlayer.Model;
 
@@ -13,19 +16,52 @@ namespace WpfMusicPlayer.Repository
 
         public async Task<MusicPlayerData> LoadMusicDataModelAsync()
         {
-            using (var reader = new StreamReader(@"C:\TestProjects\WpfMusicPlayer\WpfMusicPlayer\Assets\music.json"))
+            // First try to load file from GitHub
+            try
             {
-                var json = await reader.ReadToEndAsync();
-                return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<MusicPlayerData>(json));
+                using (var webClient = new WebClient())
+                {
+                    var jsonUri = new Uri("https://gist.githubusercontent.com/edj-boston/77b2cdc0cad5b5d42219/raw/1366c213a5b0ae29f1d29d0bc1d22d29f2586068/music.json");
+                    var jsonContents = await webClient.DownloadStringTaskAsync(jsonUri);
+                    return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<MusicPlayerData>(jsonContents));
+                }
+            }
+            catch (Exception exception)
+            {     
+                // Need better error information, logging to console for now
+                Console.WriteLine(@"Failed to load json music file asynchronously from internet, error " + exception.StackTrace);  
+                      
+                // Otherwise load it from local copy   
+                using (var reader = new StreamReader("../../Assets/music.json"))
+                {
+                    var json = await reader.ReadToEndAsync();
+                    return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<MusicPlayerData>(json));
+                }
             }
         }
 
         public MusicPlayerData LoadMusicDataModel()
         {
-            using (var reader = new StreamReader(@"C:\TestProjects\WpfMusicPlayer\WpfMusicPlayer\Assets\music.json"))
+            try
             {
-                var json = reader.ReadToEnd();
-                return JsonConvert.DeserializeObject<MusicPlayerData>(json);
+                using (var webClient = new WebClient())
+                {
+                    var jsonUri = new Uri("https://gist.githubusercontent.com/edj-boston/77b2cdc0cad5b5d42219/raw/1366c213a5b0ae29f1d29d0bc1d22d29f2586068/music.json");
+                    var jsonContents = webClient.DownloadString(jsonUri);
+                    return JsonConvert.DeserializeObject<MusicPlayerData>(jsonContents);
+                }
+            }
+            catch (Exception exception)
+            {
+                // Need better error information, logging to console for now
+                Console.WriteLine(@"Failed to load json music file from internet, error " + exception.StackTrace);
+
+                // Otherwise load it from local copy   
+                using (var reader = new StreamReader("../../Assets/music.json"))
+                {
+                    var json = reader.ReadToEnd();
+                    return JsonConvert.DeserializeObject<MusicPlayerData>(json);
+                }
             }
         }
     }
